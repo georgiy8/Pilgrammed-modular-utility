0687097488a6aad034cd39d1dd3ce19d67856c3d
georgiy8 <georgiykupriyev@gmail.com>
2026-07-05 00:43:02 +0300

    Base gui settings

--- config.lua
+++ config.lua
@@ -1,1 +1,30 @@
+-- config.lua
+local Config = {}
 
+Config.Colors = {
+    DarkGray = Color3.fromRGB(35, 35, 35),
+    MediumGray = Color3.fromRGB(50, 50, 50),
+    LightGray = Color3.fromRGB(60, 60, 60),
+    Red = Color3.fromRGB(255, 0, 0),
+    Green = Color3.fromRGB(0, 255, 0),
+    Blue = Color3.fromRGB(0, 100, 255),
+    Yellow = Color3.fromRGB(255, 255, 0),
+    Purple = Color3.fromRGB(128, 0, 128),
+    Orange = Color3.fromRGB(255, 165, 0),
+    Pink = Color3.fromRGB(255, 105, 180),
+    Cyan = Color3.fromRGB(0, 255, 255),
+}
+
+Config.ColorNames = {"DarkGray", "MediumGray", "LightGray", "Red", "Green", "Blue", "Yellow", "Purple", "Orange", "Pink", "Cyan"}
+
+Config.DefaultSettings = {
+    color1 = "DarkGray",
+    color2 = "Blue",
+    gradientEnabled = false,
+    gradientRotation = 90,
+}
+
+-- Глобальные настройки
+getgenv().PilgrammedSettings = getgenv().PilgrammedSettings or table.clone(Config.DefaultSettings)
+
+return Config
