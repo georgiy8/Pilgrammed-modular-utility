cb09a3a40421f1053d3da50d08efed2872d5c352
georgiy8 <georgiykupriyev@gmail.com>
2026-07-07 23:28:44 +0300

    Refactor Phantom Lancer module to use GitHub assets

--- modules/Phantom-lancer.lua
+++ modules/Phantom-lancer.lua
@@ -1,44 +1,51 @@
 --========================================================--
--- Phantom Lancer Module
+-- Phantom Lancer Module (GitHub Assets)
 --========================================================--
 
 return function(Window)
-
     --------------------------------------------------------
     -- Tab
     --------------------------------------------------------
-
-    local Phantom = Window:CreateTab({
-
-        Name = "Phantom Lancer"
-
+    local Visual = Window:CreateTab({
+        Name = "Phantom Lancer",
+        Icon = "⚔️"
     })
-
+    
     --------------------------------------------------------
-    -- Image
+    -- Preview Section
     --------------------------------------------------------
-
-    local Preview = Phantom:CreateSection({
-
+    local Preview = Visual:CreateSection({
         Name = "Preview"
-
     })
-
+    
+    --------------------------------------------------------
+    -- Image from GitHub
+    --------------------------------------------------------
+    local ImageUrl = "https://raw.githubusercontent.com/georgiy8/Pilgrammed-modular-utility/main/assets/phantom.png"
+    
     local PhantomImage = Preview:AddImage({
-
-        Image = getcustomasset("assets/phantom.png"),
-
+        Image = ImageUrl,
         Height = 300,
-
-        Transparency = 0,
-
-        BackgroundTransparency = 1,
-
-        ScaleType = Enum.ScaleType.Fit
-
+        AspectRatio = 16/9,
+        BackgroundTransparency = 1
     })
-
-
-    PhantomImage:SetAspectRatio(1, 1)
-
+    
+    --------------------------------------------------------
+    -- Info
+    --------------------------------------------------------
+    local Info = Visual:CreateSection({
+        Name = "Information"
+    })
+    
+    Info:AddLabel({
+        Text = "🖼️ Loaded from GitHub Repository"
+    })
+    
+    Info:AddLabel({
+        Text = "Path: assets/phantom.png"
+    })
+    
+    Info:AddLabel({
+        Text = "Make sure the file is uploaded to the repo"
+    })
 end
