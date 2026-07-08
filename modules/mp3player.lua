de3b645c23e81a8205b84cdffdc773bbe514b106
georgiy8 <georgiykupriyev@gmail.com>
2026-07-08 16:00:37 +0300

    Refactor MP3 Player module and update button texts

--- modules/mp3player.lua
+++ modules/mp3player.lua
@@ -1,5 +1,5 @@
 --========================================================--
--- MP3 Player Module
+-- Sound Test Module
 --========================================================--
 
 return function(Window)
@@ -9,19 +9,21 @@ return function(Window)
     })
     
     local Section = Tab:CreateSection({
-        Name = "Phantom Lancer Sounds"
+        Name = "Phantom Sounds"
     })
     
     local CurrentSound = nil
     
     Section:AddButton({
-        Text = "Play Last Hit Sound",
+        Text = "▶ Play Last Hit",
         Callback = function()
+            local path = "assets/Phantom-lancer/Sounds/Plance_lasthit_03_ru.mp3.mpeg"
+            
+            print("Loading path:", path)
+            
             if CurrentSound then
                 CurrentSound:Stop()
             end
-            
-            local path = "assets/Phantom-lancer/Sounds/Plance_lasthit_03_ru.mp3.mpeg"
             
             CurrentSound = Instance.new("Sound")
             CurrentSound.SoundId = getcustomasset(path)
@@ -29,7 +31,7 @@ return function(Window)
             CurrentSound.Parent = game:GetService("SoundService")
             CurrentSound:Play()
             
-            print("Playing:", path)
+            print("Playing sound...")
         end
     })
     
@@ -40,9 +42,5 @@ return function(Window)
                 CurrentSound:Stop()
             end
         end
-    })
-    
-    Section:AddLabel({
-        Text = "Path: assets/Phantom-lancer/Sounds/Plance_lasthit_03_ru.mp3.mpeg"
     })
 end
