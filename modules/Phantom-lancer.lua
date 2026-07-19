9a06e1b54893a51f72206b2fa9aeeeef0b089380
georgiy8 <georgiykupriyev@gmail.com>
2026-07-20 00:06:51 +0300

    Refactor Phantom-lancer.lua for asset handling

--- modules/Phantom-lancer.lua
+++ modules/Phantom-lancer.lua
@@ -1,59 +1,41 @@
 return function(Window)
 
+    local Assets = _G.Assets
+
     local Tab = Window:CreateTab({
-
         Name = "Asset Test",
-
         Icon = "🖼️"
-
     })
 
     local Section = Tab:CreateSection({
-
         Name = "Image Button Test"
-
     })
 
     local ImageAsset = Assets:GetImage("phantom.png")
-
     local ClickSound = Assets:GetSound("Plance_lasthit_03_ru.mp3")
 
     Section:AddLabel({
-
         Text = "Click the image."
-
     })
 
     Section:AddImage({
-
         Image = ImageAsset,
-
         Height = 320,
-
         AspectRatio = 16/9,
-
         Button = true,
-
         ClickSound = ClickSound,
 
         OnClick = function()
-
             print("Image clicked!")
-
         end,
 
         OnHover = function()
-
             print("Mouse entered.")
-
         end,
 
         OnLeave = function()
-
             print("Mouse left.")
-
         end
-
     })
 
 end
