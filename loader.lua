e36349b7489d204bb21e10c8a37fd713342778d1
georgiy8 <georgiykupriyev@gmail.com>
2026-07-20 16:57:49 +0300

    Update repository URL in loader.lua

--- loader.lua
+++ loader.lua
@@ -3,7 +3,7 @@
 -- Loader v1.0
 --========================================================--
 
-local REPO = "https://raw.githubusercontent.com/georgiy8/Pilgrammed-modular-utility/main/"
+local REPO = "https://raw.githubusercontent.com/georgiy8/Universal-modular-utility/main/"
 
 -- Простая система импорта файлов из GitHub
 local function Import(path)
